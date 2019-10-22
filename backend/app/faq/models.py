3e0b5d4fc604edf937f73b5d81f295f87c7630cb
marscj <mjx_cj@163.com>
2019-10-22 10:09:22 +0400

    commit

--- backend/app/faq/models.py
+++ backend/app/faq/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Faq(models.Models):
+    categoryId = models.IntegerField()
+    question = models.CharField()
+    answer = models.TextField()
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -50,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'casbin_middleware.middleware.CasbinMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
