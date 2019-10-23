1de4543286d52bc04352ae19d06800fab6d89f16
marscj <mjx_cj@163.com>
2019-10-23 14:59:39 +0400

    commit

--- backend/app/faq/models.py
+++ backend/app/faq/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Faq(models.Model):
     categoryId = models.IntegerField()
-    question = models.CharField()
+    question = models.CharField(null=True, blank=True, max_length=128)
     answer = models.TextField()
     
     
