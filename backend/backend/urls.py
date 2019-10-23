0372282a10f1ed0aec93452e2b5a238f1c8e990c
marscj <mjx_cj@163.com>
2019-10-23 15:14:32 +0400

    commit

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,6 +16,6 @@ Including another URLconf
 from django.conf.urls import include, url
 
 urlpatterns = [
-    url(r'^rest-auth/', include('rest_auth.urls')),
-    url(r'^rest-auth/registration/', include('rest_auth.registration.urls'))
+    url(r'^api/rest-auth/', include('rest_auth.urls')),
+    url(r'^api/rest-auth/registration/', include('rest_auth.registration.urls'))
 ]
