08ccf03da29aedc4a5d682842638d989d28419d7
marscj <mjx_cj@163.com>
2019-10-23 15:08:40 +0400

    commit

--- backend/app/user/serializers.py
+++ backend/app/user/serializers.py
@@ -1,13 +1,26 @@
-from rest_framework import serializers, exceptions
+from rest_framework import serializers
 
-from django.contrib.auth import get_user_model, authenticate
-from django.conf import settings
-from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group, Permission
 
 UserModel = get_user_model()
+
+class Permission(serializers.ModelSerializer):
+
+    class Meta:
+        model = Permission
+        fields = '__all__'
+
+class GroupSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Group
+        fields = '__all__'
 
 class UserDetailSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = UserModel
         fields = '__all__'
+
+
