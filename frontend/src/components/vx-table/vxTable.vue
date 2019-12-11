07e9cf6f2a3445e764d5bb6865412a2b16b2a911
marscj <mjx_cj@163.com>
2019-12-11 15:08:27 +0400

    commit

---
+++ frontend/src/components/vx-table/vxTable.vue
@@ -0,0 +1,255 @@
+<template>
+  <div
+    :class="[{'stripe': stripe, 'hoverFlat': hoverFlat}, `vs-table-${color}`]"
+    class="vs-component vs-con-table"
+  >
+    <!-- header -->
+    <header class="header-table vs-table--header">
+      <slot name="header"></slot>
+      <div v-if="search" class="con-input-search vs-table--search">
+        <input v-model="searchx" class="input-search vs-table--search-input" type="text" />
+        <vs-icon icon="search"></vs-icon>
+      </div>
+    </header>
+    <div class="con-tablex vs-table--content">
+      <div :style="styleConTbody" class="vs-con-tbody vs-table--tbody">
+        <table ref="table" class="vs-table vs-table--tbody-table">
+          <thead ref="thead" class="vs-table--thead">
+            <tr>
+              <th v-if="multiple || hasExpadableData" class="td-check">
+                <span v-if="multiple" class="con-td-check">
+                  <vs-checkbox
+                    :key="isCheckedLine ? 'remove' : 'check'"
+                    :icon="isCheckedLine ? 'remove' : 'check'"
+                    :checked="isCheckedMultiple"
+                    size="small"
+                    @click="changeCheckedMultiple"
+                  />
+                </span>
+              </th>
+              <slot name="thead"></slot>
+            </tr>
+          </thead>
+          <slot :data="datax"></slot>
+        </table>
+      </div>
+      <div v-if="isNoData" class="not-data-table vs-table--not-data">{{ noDataText }}</div>
+
+      <div v-if="pagination" class="con-pagination-table vs-table--pagination">
+        <vs-pagination
+          :total="getTotalPages()"
+          :maxItems="page_size"
+          :description="description"
+          :descriptionItems="descriptionItems"
+          v-model="page"
+        ></vs-pagination>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VxTable",
+  props: {
+    value: {},
+    color: {
+      default: "primary",
+      type: String
+    },
+    noDataText: {
+      default: "No data Available",
+      type: String
+    },
+    stripe: {
+      default: false,
+      type: Boolean
+    },
+    hoverFlat: {
+      default: false,
+      type: Boolean
+    },
+    maxHeight: {
+      default: "auto",
+      type: String
+    },
+    multiple: {
+      default: false,
+      type: Boolean
+    },
+    data: {
+      type: Function,
+      required: true
+    },
+    notSpacer: {
+      default: false,
+      type: Boolean
+    },
+    search: {
+      default: false,
+      type: Boolean
+    },
+    description: {
+      default: false,
+      type: Boolean
+    },
+    descriptionItems: {
+      default: () => [],
+      type: Array
+    },
+    pagination: {
+      default: false,
+      type: Boolean
+    },
+    page: {
+      default: 1,
+      type: Number
+    },
+    page_size: {
+      default: 10,
+      type: Number
+    },
+    total: {
+      default: 1,
+      type: Number
+    }
+  },
+  data: () => ({
+    headerWidth: "100%",
+    trs: [],
+    datax: [],
+    searchx: null,
+    hasExpadableData: false,
+    currentSortKey: null,
+    currentSortType: null
+  }),
+  computed: {
+    getTotalPages() {
+      return Math.ceil(this.total / this.page_size);
+    },
+    isNoData() {
+      if (typeof this.datax == Object) {
+        return this.datax
+          ? Object.keys(this.datax).length == 0
+          : false && this.search;
+      } else {
+        return this.datax ? this.datax.length == 0 : false && this.search;
+      }
+    },
+    isCheckedLine() {
+      let lengthx = this.data.length;
+      let lengthSelected = this.value.length;
+      return lengthx !== lengthSelected;
+    },
+    isCheckedMultiple() {
+      return this.value.length > 0;
+    },
+    styleConTbody() {
+      return {
+        maxHeight: this.maxHeight,
+        overflow: this.maxHeight != "auto" ? "auto" : null
+      };
+    },
+    getThs() {
+      let ths = this.$slots.thead.filter(item => item.tag);
+      return ths.length;
+    },
+    tableHeaderStyle() {
+      return {
+        width: this.headerWidth
+      };
+    }
+  },
+  watch: {
+    page() {
+      this.loadData();
+    },
+    page_size(val) {
+      this.page_size = val;
+      this.loadData();
+    },
+  },
+  mounted() {
+    window.addEventListener("resize", this.listenerChangeWidth);
+    this.maxItemsx = this.maxItems;
+    this.loadData();
+  },
+  destroyed() {
+    window.removeEventListener("resize", this.listenerChangeWidth);
+  },
+  methods: {
+    loadData() {
+      const parameter = Object.assign({page: this.page, page_size: this.page_size})
+      const result = this.data(parameter)
+
+      result.then((res) => {
+        this.datax = res.results 
+      })
+    },
+    changeCheckedMultiple() {
+      let lengthx = this.data.length;
+      let lengthSelected = this.value.length;
+      let selectedx = lengthx - lengthSelected;
+      if (selectedx == 0) {
+        this.$emit("input", []);
+      } else {
+        this.$emit("input", this.data);
+      }
+    },
+    clicktr(tr, isTr) {
+      if (this.multiple && isTr) {
+        let val = this.value.slice(0);
+        if (val.includes(tr)) {
+          val.splice(val.indexOf(tr), 1);
+        } else {
+          val.push(tr);
+        }
+
+        this.$emit("input", val);
+        this.$emit("selected", tr);
+      } else if (isTr) {
+        this.$emit("input", tr);
+        this.$emit("selected", tr);
+      }
+    },
+    dblclicktr(tr, isTr) {
+      if (isTr) {
+        this.$emit("dblSelection", tr);
+      }
+    },
+    listenerChangeWidth() {
+      this.headerWidth = `${this.$refs.table.offsetWidth}px`;
+      this.changeTdsWidth();
+    },
+    changeTdsWidth() {
+      if (!this.value) return;
+
+      let tbody = this.$refs.table.querySelector("tbody");
+
+      // Adding condition removes querySelector none error - if tbody isnot present
+      if (tbody) {
+        let trvs = tbody.querySelector(".tr-values");
+        if (trvs === undefined || trvs === null) return;
+        let tds = trvs.querySelectorAll(".td");
+
+        let tdsx = [];
+
+        tds.forEach((td, index) => {
+          tdsx.push({ index: index, widthx: td.offsetWidth });
+        });
+
+        let colgrouptable = this.$refs.colgrouptable;
+        if (colgrouptable !== undefined && colgrouptable !== null) {
+          let colsTable = colgrouptable.querySelectorAll(".col");
+          colsTable.forEach((col, index) => {
+            col.setAttribute("width", tdsx[index].widthx);
+          });
+        }
+      }
+    },
+    changeMaxItems(index) {
+      this.page_size = this.descriptionItems[index];
+    }
+  }
+};
+</script>
--- frontend/src/views/user/List.vue
+++ frontend/src/views/user/List.vue
@@ -1,238 +1,134 @@
 <template>
-  <div id="data-list-thumb-view" class="data-list-container">
-    <data-view-sidebar
-      :isSidebarActive="addNewDataSidebar"
-      @closeSidebar="toggleDataSidebar"
-      :data="sidebarData"
-    />
+    <vx-table
+      :sst="true"
+      @search="handleSearch"
+      @change-page="handleChangePage"
+      @sort="handleSort"
+      v-model="selected"
+      pagination
+      max-items="3"
+      :total="30"
+      search
+      :data="users">
 
-    <vs-table ref="table" pagination :max-items="itemsPerPage" search :data="listData">
-      <div slot="header" class="flex flex-wrap-reverse items-center flex-grow justify-between">
-        <vs-button type="border" icon-pack="feather" icon="icon-plus" @click="addNewData">Add New</vs-button>
+        <template slot="header">
+            <h3>Users</h3>
+        </template>
 
-        <!-- ITEMS PER PAGE -->
-        <vs-dropdown vs-trigger-click class="cursor-pointer mb-4 mr-4">
-          <div
-            class="p-4 border border-solid d-theme-border-grey-light rounded-full d-theme-dark-bg cursor-pointer flex items-center justify-between font-medium"
-          >
-            <span
-              class="mr-2"
-            >{{ currentPage * itemsPerPage - (itemsPerPage - 1) }} - {{ listData.length - currentPage * itemsPerPage > 0 ? currentPage * itemsPerPage : listData.length }} of {{ listData.length }}</span>
-            <feather-icon icon="ChevronDownIcon" svgClasses="h-4 w-4" />
-          </div>
-          <!-- <vs-button class="btn-drop" type="line" color="primary" icon-pack="feather" icon="icon-chevron-down"></vs-button> -->
-          <vs-dropdown-menu>
-            <vs-dropdown-item @click="itemsPerPage=5">
-              <span>5</span>
-            </vs-dropdown-item>
-            <vs-dropdown-item @click="itemsPerPage=20">
-              <span>20</span>
-            </vs-dropdown-item>
-            <vs-dropdown-item @click="itemsPerPage=50">
-              <span>50</span>
-            </vs-dropdown-item>
-            <vs-dropdown-item @click="itemsPerPage=100">
-              <span>100</span>
-            </vs-dropdown-item>
-          </vs-dropdown-menu>
-        </vs-dropdown>
-      </div>
+        <template slot="thead">
+            <vs-th sort-key="email">Email</vs-th>
+            <vs-th sort-key="username">Name</vs-th>
+            <vs-th sort-key="website">Website</vs-th>
+            <vs-th sort-key="id">Nro</vs-th>
+        </template>
 
-      <template slot="thead">
-        <vs-th sort-key="username">Name</vs-th>
-        <vs-th>Action</vs-th>
-      </template>
-
-      <template slot-scope="{data}">
-        <tbody>
-          <vs-tr :data="tr" :key="indextr" v-for="(tr, indextr) in data">
-            <vs-td>
-              <p class="product-name font-medium truncate">{{ tr.codename }}</p>
-            </vs-td>
-            <vs-td class="whitespace-no-wrap">
-              <feather-icon
-                icon="EditIcon"
-                svgClasses="w-5 h-5 hover:text-primary stroke-current"
-                @click.stop="editData(tr)"
-              />
-              <feather-icon
-                icon="TrashIcon"
-                svgClasses="w-5 h-5 hover:text-danger stroke-current"
-                class="ml-2"
-                @click.stop="deleteData(tr.id)"
-              />
-            </vs-td>
-          </vs-tr>
-        </tbody>
-      </template>
-    </vs-table>
-  </div>
+        <template slot-scope="{data}">
+            <vs-tr :data="tr" :key="indextr" v-for="(tr, indextr) in data">
+                <vs-td :data="data[indextr].email">
+                    {{ data[indextr].email }}
+                </vs-td>
+                <vs-td :data="data[indextr].username">
+                    {{ data[indextr].username }}
+                </vs-td>
+                <vs-td :data="data[indextr].id">
+                    {{ data[indextr].website }}
+                </vs-td>
+                <vs-td :data="data[indextr].id">
+                    {{ data[indextr].id }}
+                </vs-td>
+            </vs-tr>
+        </template>
+    </vx-table>
 </template>
 
 <script>
-import DataViewSidebar from "./DataViewSidebar.vue";
-import { getUsers, getPermission } from "@/http/requests/user/index.js";
-
 export default {
-  components: {
-    DataViewSidebar
-  },
-  data() {
-    return {
-      isMounted: false,
-      itemsPerPage: 5,
-      listData: [],
-      addNewDataSidebar: false,
-      sidebarData: {}
-    };
-  },
-  computed: {
-    currentPage() {
-      if (this.isMounted) {
-        return this.$refs.table.currentx;
+  data:()=>({
+    selected:[],
+    log: [],
+    users:[
+      {
+        "id": 1,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "website": "hildegard.org",
+      },
+      {
+        "id": 2,
+        "name": "Ervin Howell",
+        "username": "Antonette",
+        "email": "Shanna@melissa.tv",
+        "website": "anastasia.net",
+      },
+      {
+        "id": 3,
+        "name": "Clementine Bauch",
+        "username": "Samantha",
+        "email": "Nathan@yesenia.net",
+        "website": "ramiro.info",
+      },
+      {
+        "id": 4,
+        "name": "Patricia Lebsack",
+        "username": "Karianne",
+        "email": "Julianne.OConner@kory.org",
+        "website": "kale.biz",
+      },
+      {
+        "id": 5,
+        "name": "Chelsey Dietrich",
+        "username": "Kamren",
+        "email": "Lucio_Hettinger@annie.ca",
+        "website": "demarco.info",
+      },
+      {
+        "id": 6,
+        "name": "Mrs. Dennis Schulist",
+        "username": "Leopoldo_Corkery",
+        "email": "Karley_Dach@jasper.info",
+        "website": "ola.org",
+      },
+      {
+        "id": 7,
+        "name": "Kurtis Weissnat",
+        "username": "Elwyn.Skiles",
+        "email": "Telly.Hoeger@billy.biz",
+        "website": "elvis.io",
+      },
+      {
+        "id": 8,
+        "name": "Nicholas Runolfsdottir V",
+        "username": "Maxime_Nienow",
+        "email": "Sherwood@rosamond.me",
+        "website": "jacynthe.com",
+      },
+      {
+        "id": 9,
+        "name": "Glenna Reichert",
+        "username": "Delphine",
+        "email": "Chaim_McDermott@dana.io",
+        "website": "conrad.com",
+      },
+      {
+        "id": 10,
+        "name": "Clementina DuBuque",
+        "username": "Moriah.Stanton",
+        "email": "Rey.Padberg@karina.biz",
+        "website": "ambrose.net",
       }
-      return 0;
-    }
-    // products() {
-    //   return []
-    // }
-  },
-  methods: {
-    getList() {
-      getPermission().then(res => {
-        const { result } = res;
-        this.listData = result.results;
-        this.listData.length = 52
-      });
+    ]
+  }),
+  methods:{
+    handleSearch(searching) {
+      console.log(`The user searched for: ${searching}`)
     },
-
-    addNewData() {
-      this.sidebarData = {};
-      this.toggleDataSidebar(true);
+    handleChangePage(page) {
+      console.log(`The user changed the page to: ${page}`)
     },
-    deleteData(id) {},
-    editData(data) {
-      this.sidebarData = data;
-      this.toggleDataSidebar(true);
-    },
-    getOrderStatusColor(status) {
-      return "primary";
-    },
-    getPopularityColor(num) {
-      return "primary";
-    },
-    toggleDataSidebar(val = false) {
-      this.addNewDataSidebar = val;
-    }
-  },
-  created() {},
-  mounted() {
-    console.log(this.$refs.table);
-    this.isMounted = true
-    this.getList();
-  },
-  watch: {
-    "$refs.table": (newValue, oldValue) => {
-      console.log("1111111");
-    }
-  }
-};
-</script>
-
-<style lang="scss">
-#data-list-thumb-view {
-  .vs-con-table {
-    .product-name {
-      max-width: 23rem;
-    }
-
-    .vs-table--header {
-      display: flex;
-      flex-wrap: wrap-reverse;
-      margin-left: 1.5rem;
-      margin-right: 1.5rem;
-      > span {
-        display: flex;
-        flex-grow: 1;
-      }
-
-      .vs-table--search {
-        padding-top: 0;
-
-        .vs-table--search-input {
-          padding: 0.9rem 2.5rem;
-          font-size: 1rem;
-
-          & + i {
-            left: 1rem;
-          }
-
-          &:focus + i {
-            left: 1rem;
-          }
-        }
-      }
-    }
-
-    .vs-table {
-      border-collapse: separate;
-      border-spacing: 0 1.3rem;
-      padding: 0 1rem;
-
-      tr {
-        box-shadow: 0 4px 20px 0 rgba(0, 0, 0, 0.05);
-        td {
-          padding: 10px;
-          &:first-child {
-            border-top-left-radius: 0.5rem;
-            border-bottom-left-radius: 0.5rem;
-          }
-          &:last-child {
-            border-top-right-radius: 0.5rem;
-            border-bottom-right-radius: 0.5rem;
-          }
-          &.img-container {
-            // width: 1rem;
-            // background: #fff;
-
-            span {
-              display: flex;
-              justify-content: flex-start;
-            }
-
-            .product-img {
-              height: 110px;
-            }
-          }
-        }
-        td.td-check {
-          padding: 20px !important;
-        }
-      }
-    }
-
-    .vs-table--thead {
-      th {
-        padding-top: 0;
-        padding-bottom: 0;
-
-        .vs-table-text {
-          text-transform: uppercase;
-          font-weight: 600;
-        }
-      }
-      th.td-check {
-        padding: 0 15px !important;
-      }
-      tr {
-        background: none;
-        box-shadow: none;
-      }
-    }
-
-    .vs-table--pagination {
-      justify-content: center;
+    handleSort(key, active) {
+      console.log(`the user ordered: ${key} ${active}`)
     }
   }
 }
-</style>
+</script>
