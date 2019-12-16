b61783574afefe85049a6434012645fc086a163e
marscj <mjx_cj@163.com>
2019-12-16 16:25:14 +0400

    commit

--- backend/app/user/models.py
+++ backend/app/user/models.py
@@ -2,12 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, Group
 
 class User(AbstractUser):
-    
-    # class UserType(models.IntegerChoices):
-    #     Customer = 1
-    #     Driver = 2
-    #     Operator = 3
-    #     Accounting = 4
 
     company = models.CharField(null=True, blank=True, max_length=64)
 
@@ -16,8 +10,6 @@ class User(AbstractUser):
     name = models.CharField(null=True, blank=True, max_length=150)
 
     phone = models.CharField(null=True, blank=True, max_length=16)
-
-    # user_type = models.IntegerField(choices=UserType.choices, default=UserType.Customer)
     
     class Meta:
         db_table = 'user'
