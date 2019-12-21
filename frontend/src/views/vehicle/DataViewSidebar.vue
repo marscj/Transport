4866a05a715d02998530965dd9384945eae2ea7b
marscj <mjx_cj@163.com>
2019-12-21 15:31:38 +0400

    commit

---
+++ frontend/src/views/vehicle/DataViewSidebar.vue
@@ -0,0 +1,220 @@
+<template>
+  <vs-sidebar
+    click-not-close
+    position-right
+    parent="body"
+    default-index="1"
+    color="primary"
+    class="add-new-data-sidebar items-no-padding"
+    spacer
+    v-model="isSidebarActiveLocal"
+  >
+    <div class="mt-6 flex items-center justify-between px-6">
+      <h4>{{ isEdit ? "ADD NEW" : "UPDATE" }} ITEM</h4>
+      <feather-icon icon="XIcon" @click.stop="isSidebarActiveLocal = false" class="cursor-pointer"></feather-icon>
+    </div>
+    <vs-divider class="mb-0"></vs-divider>
+
+    <VuePerfectScrollbar class="scroll-area--data-list-add-new" :settings="settings">
+      <div class="p-6">
+        <validation-observer ref="observer" v-slot="{ validate, dirty }">
+          <validation-provider name="username" rules="required|max:16|min:5" v-slot="{ errors }">
+            <vs-input
+              data-vv-validate-on="blur"
+              label="Username"
+              :disabled="!isEdit"
+              v-model="form.username"
+              class="mt-5 w-full"
+            />
+            <span>{{ errors[0] }}</span>
+          </validation-provider>
+
+          <validation-provider name="email" rules="email|required" v-slot="{ errors }">
+            <vs-input
+              data-vv-validate-on="blur"
+              label="Email"
+              :disabled="!isEdit"
+              v-model="form.email"
+              class="mt-5 w-full"
+            />
+            <span>{{ errors[0] }}</span>
+          </validation-provider>
+
+          <validation-provider name="name" rules v-slot="{ errors }">
+            <vs-input
+              data-vv-validate-on="blur"
+              label="Name"
+              v-model="form.name"
+              class="mt-5 w-full"
+            />
+            <span>{{ errors[0] }}</span>
+          </validation-provider>
+
+          <validation-provider name="phone" rules v-slot="{ errors }">
+            <vs-input
+              data-vv-validate-on="blur"
+              label="Phone"
+              v-model="form.phone"
+              class="mt-5 w-full"
+            />
+            <span>{{ errors[0] }}</span>
+          </validation-provider>
+
+          <validation-provider name="company" rules v-slot="{ errors }">
+            <vs-input
+              data-vv-validate-on="blur"
+              label="Company"
+              v-model="form.company"
+              class="mt-5 w-full"
+            />
+            <span>{{ errors[0] }}</span>
+          </validation-provider>
+
+          <vs-checkbox v-model="form.is_superuser" class="mt-5 w-full">Admin</vs-checkbox>
+
+          <vs-checkbox v-model="form.is_active" class="mt-5 w-full">Active</vs-checkbox>
+
+          <vs-select label="Groups" class="w-full mt-5" v-model="form.group_id">
+            <vs-select-item
+              :key="index"
+              :value="item"
+              :text="item.name"
+              v-for="(item,index) in groups"
+            />
+          </vs-select>
+
+          <validation-provider name="non_field_errors" v-slot="{ errors }">
+            <span>{{ errors[0] }}</span>
+          </validation-provider>
+        </validation-observer>
+      </div>
+    </VuePerfectScrollbar>
+
+    <div class="flex flex-wrap items-center p-6" slot="footer">
+      <vs-button class="mr-6" @click="submit">Submit</vs-button>
+      <vs-button type="border" color="danger" @click="isSidebarActiveLocal = false">Cancel</vs-button>
+    </div>
+  </vs-sidebar>
+</template>
+
+<script>
+import VuePerfectScrollbar from "vue-perfect-scrollbar";
+import {
+  updateUser,
+  createUser,
+  getGroup
+} from "@/http/requests/user/index.js";
+
+export default {
+  components: {
+    VuePerfectScrollbar
+  },
+  props: {
+    isSidebarActive: {
+      type: Boolean,
+      required: true
+    },
+    data: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  watch: {
+    isSidebarActive(val) {
+      if (!val) return;
+      this.form = Object.assign({}, this.data);
+    }
+  },
+  computed: {
+    isSidebarActiveLocal: {
+      get() {
+        return this.isSidebarActive;
+      },
+      set(val) {
+        if (!val) {
+          this.$emit("closeSidebar");
+        }
+      }
+    },
+    isEdit() {
+      return Object.entries(this.data).length === 0;
+    }
+  },
+  data() {
+    return {
+      form: {
+        id: undefined,
+        username: "",
+        email: "",
+        name: "",
+        phone: "",
+        is_superuser: false,
+        is_active: false,
+        groups: [],
+        group_id: [],
+        company: ""
+      },
+      groups: [],
+      settings: {
+        maxScrollbarLength: 60,
+        wheelSpeed: 0.6
+      }
+    };
+  },
+  mounted() {
+    if (Object.entries(this.data).length === 0) {
+      this.getGroup();
+    }
+  },
+  methods: {
+    submit() {
+      if (this.isEdit) {
+        createUser(this.form).then(res => {
+          this.isSidebarActiveLocal = false;
+        });
+      } else {
+        updateUser(this.form.id, this.form).then(res => {
+          this.isSidebarActiveLocal = false;
+        });
+      }
+    },
+    getGroup() {
+      getGroup().then(res => {
+        const { result } = res;
+        this.groups = result;
+      });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.add-new-data-sidebar {
+  ::v-deep .vs-sidebar--background {
+    z-index: 52010;
+  }
+
+  ::v-deep .vs-sidebar {
+    z-index: 52010;
+    width: 400px;
+    max-width: 90vw;
+
+    .img-upload {
+      margin-top: 2rem;
+
+      .con-img-upload {
+        padding: 0;
+      }
+
+      .con-input-upload {
+        width: 100%;
+        margin: 0;
+      }
+    }
+  }
+}
+
+.scroll-area--data-list-add-new {
+  height: calc(100% - 4.3rem);
+}
+</style>
--- frontend/src/views/vehicle/List.vue
+++ frontend/src/views/vehicle/List.vue
@@ -1,3 +1,206 @@
 <template>
-	<h4>This is Vehicle Page.</h4>
+  <vs-card id="data-list-thumb-view" class="data-list-container">
+    <data-view-sidebar
+      :isSidebarActive="addNewDataSidebar"
+      @closeSidebar="toggleDataSidebar"
+      :data="sidebarData"
+    />
+
+    <vx-table ref="table" pagination search :data="loadData" :page_size="page_size">
+      <div slot="header" class="flex flex-wrap-reverse items-center flex-grow justify-between">
+        <vs-button type="border" icon-pack="feather" icon="icon-plus" @click="addNewData">Add New</vs-button>
+
+        <vs-dropdown vs-trigger-click class="cursor-pointer mr-4">
+          <div
+            class="p-4 border border-solid d-theme-border-grey-light rounded-full d-theme-dark-bg cursor-pointer flex items-center justify-between font-medium"
+          >
+            <span
+              class="mr-6"
+            >{{ currentPage * page_size - (page_size - 1) }} - {{ total - currentPage * page_size > 0 ? currentPage * page_size : total }}</span>
+            <feather-icon icon="ChevronDownIcon" svgClasses="h-4 w-4" />
+          </div>
+          <vs-dropdown-menu>
+            <vs-dropdown-item @click="page_size=10">
+              <span>10</span>
+            </vs-dropdown-item>
+            <vs-dropdown-item @click="page_size=20">
+              <span>20</span>
+            </vs-dropdown-item>
+            <vs-dropdown-item @click="page_size=50">
+              <span>50</span>
+            </vs-dropdown-item>
+            <vs-dropdown-item @click="page_size=100">
+              <span>100</span>
+            </vs-dropdown-item>
+          </vs-dropdown-menu>
+        </vs-dropdown>
+      </div>
+
+      <template slot="thead">
+        <vs-th style-key="id" style="width: 80px;">ID</vs-th>
+        <vs-th style-key="license_plate">license plate</vs-th>
+        <vs-th style-key="seat">seat</vs-th>
+        <vs-th style-key="driver">driver</vs-th>
+        <vs-th style-key="seats">seats</vs-th>
+        <vs-th style-key="is_active">active</vs-th>
+      </template>
+
+      <template slot-scope="{data}">
+        <vs-tr :data="tr" :key="indextr" v-for="(tr, indextr) in data" :activeEdit="true">
+          <vs-td :data="tr.id">{{ tr.id }}</vs-td>
+          <vs-td :data="tr.license_plate">
+            <a @click="editData(tr)">{{ tr.license_plate }}</a>
+          </vs-td>
+          <vs-td :data="tr.seat">
+            <a @click="editData(tr)">{{ tr.seat }}</a>
+          </vs-td>
+          <vs-td :data="tr.driver">
+            <a @click="editData(tr)">{{ tr.driver.username }}</a>
+          </vs-td>
+          <vs-td :data="tr.seats">
+            <a @click="editData(tr)">{{ tr.driver.seats.name }}</a>
+          </vs-td>
+          <vs-td :data="tr.is_active">
+            <vs-checkbox v-model="tr.is_active" :disabled="true" style="float:left;" />
+          </vs-td>
+        </vs-tr>
+      </template>
+    </vx-table>
+  </vs-card>
 </template>
+
+<script>
+import { getVehicle } from "@/http/requests/vehicle/index.js";
+import DataViewSidebar from "./DataViewSidebar.vue";
+
+const UserType = [
+  { value: 1, label: "Customer" },
+  { value: 2, label: "Driver" },
+  { value: 3, label: "Operator" },
+  { value: 4, label: "Accounting" }
+];
+
+export default {
+  components: {
+    DataViewSidebar
+  },
+  data() {
+    return {
+      UserType,
+      isMounted: false,
+      page_size: 10,
+      addNewDataSidebar: false,
+      sidebarData: {},
+      selected: [],
+      loadData: parameter => {
+        return getVehicle(Object.assign(parameter, {})).then(res => {
+          return res.result;
+        });
+      }
+    };
+  },
+  computed: {
+    currentPage() {
+      return this.isMounted ? this.$refs.table.page : 0;
+    },
+    total() {
+      return this.isMounted ? this.$refs.table.total : 0;
+    }
+  },
+  mounted() {
+    this.isMounted = true;
+  },
+  methods: {
+    addNewData() {
+      this.sidebarData = {};
+      this.toggleDataSidebar(true);
+    },
+    editData(data) {
+      this.sidebarData = data;
+      this.toggleDataSidebar(true);
+    },
+    toggleDataSidebar(val = false) {
+      this.addNewDataSidebar = val;
+      this.$refs.table.refresh();
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+#data-list-thumb-view {
+  .vs-con-table {
+    .vs-table--header {
+      display: flex;
+      flex-wrap: wrap-reverse;
+      margin-left: 1rem;
+      margin-right: 1rem;
+      > span {
+        display: flex;
+        flex-grow: 1;
+      }
+
+      .vs-table--search {
+        padding-top: 2;
+
+        .vs-table--search-input {
+          padding: 0.9rem 2.5rem;
+          font-size: 1rem;
+
+          & + i {
+            left: 1rem;
+          }
+
+          &:focus + i {
+            left: 1rem;
+          }
+        }
+      }
+    }
+
+    .vs-table {
+      border-collapse: collapse;
+      // border-collapse: separate;
+      // border-spacing: 10px 2px;
+      // padding: 10px;
+      tr {
+        // box-shadow: 0 1px 10px 0 rgba(0, 0, 0, 0.05);
+        td {
+          padding: 20px;
+        }
+        td.td-check {
+          padding: 20px !important;
+        }
+      }
+      th,
+      tr,
+      td {
+        border: 1px solid #e8e8e8;
+      }
+    }
+
+    .vs-table--thead {
+      th {
+        padding-top: 20;
+        padding-bottom: 20;
+
+        .vs-table-text {
+          text-transform: uppercase;
+          font-weight: 600;
+        }
+      }
+      th.td-check {
+        padding: 0 15px !important;
+      }
+      tr {
+        background: none;
+        box-shadow: none;
+      }
+    }
+
+    .vs-table--pagination {
+      justify-content: center;
+    }
+  }
+}
+</style>
