b4dcccd2abf5d1da54bdb6f2d118c6e400b8b856
marscj <mjx_cj@163.com>
2019-12-21 11:50:03 +0400

    commit

--- backend/app/vehicle/urls.py
+++ backend/app/vehicle/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from .import views
 
 router = DefaultRouter()
-router.register(r'itinerarys', views.ItineraryView, basename='itinerary')
+router.register(r'itineraryies', views.ItineraryView, basename='itinerary')
 router.register(r'seats', views.SeatView, basename='seat')
 router.register(r'prices', views.PriceView, basename='price')
 router.register(r'vehices', views.VehicleView, basename='vehicle')
--- frontend/src/http/requests/vehicle/index.js
+++ frontend/src/http/requests/vehicle/index.js
@@ -3,37 +3,58 @@ import {
 } from '@/utils/request'
 
 export function getItinerary(parameter) {
-  return axios.get("users/", {
+  return axios.get("itineraryies/", {
     params: parameter
   })
 }
 
-export function updateUser(pk, data) {
-  return axios.put("users/" + `${pk}/`, data)
+export function updateItinerary(pk, data) {
+  return axios.put("itineraryies/" + `${pk}/`, data)
 }
 
-export function createUser(data) {
-  return axios.post("users/", data)
+export function createItinerary(data) {
+  return axios.post("itineraryies/", data)
 }
 
-export function getGroup() {
-  return axios.get("groups/")
-}
 
-export function updateGroups(pk, data) {
-  return axios.put("groups/" + `${pk}/`, data)
-}
-
-export function createGroup(data) {
-  return axios.post("groups/", data)
-}
-
-export function getPermission(parameter) {
-  return axios.get("permissions/", {
+export function getSeat(parameter) {
+  return axios.get("seats/", {
     params: parameter
   })
 }
 
-export function updateGroup(pk, data) {
-  return axios.put("groups/" + `${pk}/`, data)
+export function updateSeat(pk, data) {
+  return axios.put("seats/" + `${pk}/`, data)
+}
+
+export function createSeat(data) {
+  return axios.post("seats/", data)
+}
+
+export function getPrice(parameter) {
+  return axios.get("prices/", {
+    params: parameter
+  })
+}
+
+export function updatePrice(pk, data) {
+  return axios.put("prices/" + `${pk}/`, data)
+}
+
+export function createPrice(data) {
+  return axios.post("prices/", data)
+}
+
+export function getVehicle(parameter) {
+  return axios.get("vehices/", {
+    params: parameter
+  })
+}
+
+export function updateVehicle(pk, data) {
+  return axios.put("vehices/" + `${pk}/`, data)
+}
+
+export function createVehicle(data) {
+  return axios.post("vehices/", data)
 }
