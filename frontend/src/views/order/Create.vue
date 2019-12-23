c402eb8f2737ce17fa9e9e74b5005785542ba014
marscj <mjx_cj@163.com>
2019-12-23 15:36:18 +0400

    commit

--- frontend/src/views/order/Create.vue
+++ frontend/src/views/order/Create.vue
@@ -87,7 +87,7 @@
           </div>
           <div class="flex flex-wrap mt-10">
             <button
-              class="ml-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-10 rounded"
+              class="ml-auto bg-blue-500 hover:bg-blue-700 focus:outline-none text-white font-bold py-2 px-10 rounded"
             >Submit</button>
           </div>
         </div>
