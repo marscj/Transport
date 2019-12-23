6aab3bcf7894c0598cc481628d97b1ec00a6d1c3
marscj <mjx_cj@163.com>
2019-12-23 17:54:46 +0400

    commit

--- backend/app/menu/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Menu(models.Model):
-
-    name = models.CharField(blank=True, null=True, max_length=64)
-    
-    hidden = models.BooleanField(default=False)
-
-    child = models.ForeignKey('Menu', on_delete=models.SET_NULL, related_name='menu', blank=True, null=True)
