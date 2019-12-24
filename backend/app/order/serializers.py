cd02ff363e4f0ad5ec3bc91822a52f33389ccae1
marscj <mjx_cj@163.com>
2019-12-24 10:02:35 +0400

    commit

--- backend/app/order/serializers.py
+++ backend/app/order/serializers.py
@@ -2,14 +2,32 @@ from rest_framework import serializers
 
 from .models import Order, OrderItinerary
 
+class OrderItinerarySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = OrderItinerary
+        fields = '__all__'
+
 class OrderSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Order
         fields = '__all__'
 
-class OrderItinerarySerializer(serializers.ModelSerializer):
+class OrderCreateSerializer(serializers.ModelSerializer):
+
+    start_date = serializers.DateField(required=False)
+
+    end_date = serializers.DateField(required=False)
+
+    category = serializers.CharField(required=False, max_length=64)
+
+    seats = serializers.IntegerField(required=False, min_value=2)
+
+    itinerary = serializers.CharField(required=False, max_length=1024)
 
     class Meta:
-        model = OrderItinerary
+        model = Order
         fields = '__all__'
+
+    
--- backend/app/order/views.py
+++ backend/app/order/views.py
@@ -2,11 +2,13 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from middleware.permission import CustomModelPermissions
 
+from middleware.mixin import CreateSerializerMixin
 from .models import Order, OrderItinerary
-from .serializers import OrderSerializer, OrderItinerarySerializer
+from .serializers import OrderSerializer, OrderCreateSerializer, OrderItinerarySerializer
 
-class OrderView(ModelViewSet):
+class OrderView(ModelViewSet, CreateSerializerMixin):
     serializer_class = OrderSerializer
+    serializer_create_class = OrderCreateSerializer
     permission_classes = [IsAuthenticated, CustomModelPermissions]
     queryset = Order.objects.all()
 
