dc69ed97c86bf6916bf4391e06f3ca009c4b3f77
marscj <mjx_cj@163.com>
2019-12-24 09:28:20 +0400

    commit

--- frontend/src/layouts/main/Main.vue
+++ frontend/src/layouts/main/Main.vue
@@ -82,27 +82,6 @@
                   v-if="$route.meta.breadcrumb"
                   :route="$route"
                 />
-
-                <!-- DROPDOWN -->
-                <!-- <vs-dropdown vs-trigger-click class="ml-auto md:block hidden cursor-pointer">
-                  <vs-button radius icon="icon-settings" icon-pack="feather" />
-
-                  <vs-dropdown-menu class="w-32">
-                    <vs-dropdown-item>
-                      <div
-                        @click="$router.push('/profile').catch(() => {})"
-                        class="flex items-center"
-                      >
-                        <feather-icon
-                          icon="UserIcon"
-                          class="inline-block mr-2"
-                          svgClasses="w-4 h-4"
-                        />
-                        <span>Profile</span>
-                      </div>
-                    </vs-dropdown-item>
-                  </vs-dropdown-menu>
-                </vs-dropdown> -->
               </div>
             </transition>
 
