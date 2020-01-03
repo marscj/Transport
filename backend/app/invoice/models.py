3bc2deaf972865c25a275955e82549e4841078c9
marscj <mjx_cj@163.com>
2020-01-03 20:45:41 +0400

    commit

--- backend/app/invoice/models.py
+++ backend/app/invoice/models.py
@@ -2,10 +2,6 @@ from django.db import models
 
 class Invoice(models.Model):
 
-    invoiceNo = models.CharField(blank=True, null=True, max_length=64)
-
-    date = models.DateField(blank=True, null=True)
-
-    create_at = models.DateTimeField(auto_now_add=True)
+    create_at = models.DateField(auto_now_add=True)
 
     change_at = models.DateTimeField(auto_now=True)
