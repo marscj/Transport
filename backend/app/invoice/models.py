a9bcb8efdd2ff717e2abca7671982b02ce923ed4
marscj <mjx_cj@163.com>
2020-01-06 10:12:44 +0400

    commit

--- backend/app/invoice/models.py
+++ backend/app/invoice/models.py
@@ -2,6 +2,17 @@ from django.db import models
 
 class Invoice(models.Model):
 
+    class Status(models.TextChoices):
+        Unpaid = 'Unpaid'
+        Paid = 'Paid'
+
     create_at = models.DateField(auto_now_add=True)
 
     change_at = models.DateTimeField(auto_now=True)
+
+    status = models.CharField(default=Status.Unpaid, choices=Status.choices, max_length=16, blank=True, null=True)
+
+    start_date = models.DateField(blank=True, null=True)
+
+    end_date = models.DateField(blank=True, null=True)
+
